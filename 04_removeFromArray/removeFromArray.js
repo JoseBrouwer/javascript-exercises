ba25dc2ef775747378b0db5980de0fbbe8002db4
JoseBrouwer <josebrouwer1@gmail.com>
2024-09-27 15:36:10 -0400

    Exercise 3 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for(const arg of args) //for every argument to remove
+    {
+        for(let i = 0; i < arr.length; i++) //for every element in the array
+        {
+            if(arg === arr[i]) //if arg in arr
+            {
+                arr.splice(arr.indexOf(arr[i]), 1)
+                i = 0; //start from beggining if modified
+            } 
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
