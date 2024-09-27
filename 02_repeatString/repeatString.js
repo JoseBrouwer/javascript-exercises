df734d630c518861f45ac7e5b83cd91fd08f1991
JoseBrouwer <josebrouwer1@gmail.com>
2024-09-27 13:48:33 -0400

    Exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeat, count) {
+    let sentence = "";
+    if(count < 0)
+        return "ERROR"
+    for(let i = 0; i < count; i++)
+    {
+        sentence += repeat;
+    }
+    return sentence;
 };
 
 // Do not edit below this line
