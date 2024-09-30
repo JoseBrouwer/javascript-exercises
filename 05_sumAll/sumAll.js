87c4ed8b704e1511dad03ec16d862b69c87d90a9
JoseBrouwer <josebrouwer1@gmail.com>
2024-09-30 09:48:49 -0400

    Exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if((isNaN(start)) || (isNaN(end))) //if params are not numbers
+        return "ERROR";
+    else if((!Number.isInteger(start)) || (!Number.isInteger(end))) //if params are not ints
+        return "ERROR";
+    else if((start < 0) || (end < 0)) //if params are negative
+        return "ERROR";
+    else if(start > end) //swap
+    {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let sum = 0;
+    for(let i = start; i <= end; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
