0f57b0879df5fb55a2463e7733fa090753bc7456
JoseBrouwer <josebrouwer1@gmail.com>
2024-12-05 09:51:06 -0500

    Exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    let count;
+    if(typeof(count) !== "number")
+        count = parseInt(n);
+    else
+        count = n;
+    if(count < 0)
+        return "OOPS";
+    else if (count == 0)
+        return 0;
+
+    let n_1 = 1;
+    let n_2 = 0;
+    let i = 2;
+    while(i <= count)
+    {
+        let fn = n_1 + n_2;
+        n_2 = n_1;
+        n_1 = fn;
+        i++;
+    }
+    return n_1;
 };
 
 // Do not edit below this line
