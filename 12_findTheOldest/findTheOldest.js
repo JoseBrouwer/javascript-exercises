b6a42aedf97b5bb09f6ed445340e5dc9624e5387
JoseBrouwer <josebrouwer1@gmail.com>
2024-12-05 11:35:07 -0500

    Exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = 0;
+    people.forEach(person => {
+        if(!person.yearOfDeath)
+        {
+            let today = new Date(2024, 11); //assuming 0 index since 12 makes 2025
+            person.yearOfDeath = today.getFullYear();
+        }
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if(age > oldest)
+            oldest = age;
+    });
+    let oldestPerson = people.find(person => (person.yearOfDeath - person.yearOfBirth) === oldest);
+    return oldestPerson;
 };
 
 // Do not edit below this line
